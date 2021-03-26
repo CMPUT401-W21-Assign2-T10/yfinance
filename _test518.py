d33091d5627746e28498901b1ac681280b91f34e
fpunjani <fpunjani@ualberta.ca>
2021-03-26 15:15:31 -0400

    update test518 + quarterly cashflow
    - wrote tests for 4 methods
    - documentation pending

--- _test518.py
+++ _test518.py
@@ -6,15 +6,28 @@ import yfinance as yf
 symbols = ['MSFT', 'IWO', 'VFINX', '^GSPC', 'BTC-USD']
 tickers = [yf.Ticker(symbol) for symbol in symbols]
 
-class TestTicker:
+class TestTicker(unittest.TestCase):
     def test_info_history(self):
         for ticker in tickers:
             # always should have info and history for valid symbols
             assert(ticker.info is not None and ticker.info != {})
             assert(ticker.history(period="max").empty is False)
 
-    def test_fiancials(self):
+    def test_financials(self):
         for ticker in tickers:
             assert(ticker.financials.empty is False)
-            assert len(ticker.financials()) == 0
+
+    def test_balance_sheet(self):
+        for ticker in tickers:
+            assert(ticker.balance_sheet.empty is False)
+    
+    def test_cashflow(self):
+        for ticker in tickers:
+            assert(ticker.cashflow.empty is False)
+    
+    def test_quarterlycashflow(self):
+        for ticker in tickers:
+            assert(ticker.quarterly_cashflow.empty is False)
+         
+         
             
